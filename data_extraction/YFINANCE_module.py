a32013339bb43d8c1cea7bb373962de62c47f16f
EliaLand <e.lando2001@gmail.com>
2025-11-18 19:43:47 +0900

    hourly data nasdaq

--- data_extraction/YFINANCE_module.py
+++ data_extraction/YFINANCE_module.py
@@ -11,15 +11,17 @@ import yfinance as yf
 # symbol: Ticker symbol (type=str, ex. "AAPL" or even "EURUSD" for exchange rate)
 # start: start date (format YYYY-MM-DD, type=str, ex. "1947-01-01")
 # end: end date (format YYYY-MM-DD, type=str, ex. "2025-04-01")
-# (!!!) filter for frequency (3 options: daily (1d), weekly (1wk), monthly (1mo))
+# (!!!) filter for frequency (4 options: hourly (1h), daily (1d), weekly (1wk), monthly (1mo))
 def fetch_YFINANCE(symbol: str, start: str, end: str, frequency: str):
-# Frequency settings (1d/1wk/1mo) and progress=False to not dipslay the progress bar
+# Frequency settings (1h/1d/1wk/1mo) and progress=False to not dipslay the progress bar
     data = yf.download(symbol, start=start, end=end, interval=frequency, progress=False)
     data = data.reset_index()
 
 # (!!!) For better readibility we drop second level index, in this case the ticker (panel data for single stocks will be handle differently)
     if isinstance(data.columns, pd.MultiIndex):
         data.columns = data.columns.droplevel(1)
-
-    data = data[["Date", "Open", "Close", "High", "Low", "Volume"]]
+    if frequency == "1h": 
+        data = data[["Datetime", "Open", "Close", "High", "Low", "Volume"]]
+    else:
+        data = data[["Date", "Open", "Close", "High", "Low", "Volume"]]
     return data
